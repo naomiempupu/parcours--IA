24e794543e77b2d2e660cfe6e334ea06357c1dba
naomie <naomiempupu@gmail.com>
2026-06-16 17:00:47 +0200

    on arrete la boucle si le nombre choisi est bon

--- guessnum.py
+++ guessnum.py
@@ -5,3 +5,8 @@ tentatives = 0
 while True:
     choix = int(input("Choisissez un nombre entre 1 et 100 : "))
     tentatives += 1
+    if choix == devine:
+        print(f"Félicitations ! Vous avez trouvé le nombre en {tentatives} tentatives.")
+        break
+
+    
