846683d9868cbb32678c2690945d705761bb2486
naomie <naomiempupu@gmail.com>
2026-06-16 15:41:36 +0200

    condition

--- guessnum.py
+++ guessnum.py
@@ -3,3 +3,12 @@ cache = random.randint(1, 1000)
 essai = 0
 print("listez le nombre entre 1 et 1000 ")
 trouve = False
+
+while not trouve:
+    nombre = int(input("Entrez un nombre : "))
+    essais += 1
+    
+if nombre < cache:
+        print("Le nombre secret est plus grand.")
+elif nombre > cache:
+        print("Le nombre secret est plus petit.")
