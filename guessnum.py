79497934fde1b3b369d30d2e705a92979f0955f8
naomie <naomiempupu@gmail.com>
2026-06-16 16:22:42 +0200

    modification du nombre à deviner

--- guessnum.py
+++ guessnum.py
@@ -1,18 +1,2 @@
 import random
-cache = random.randint(1, 1000)
-essai = 0
-print("listez le nombre entre 1 et 1000 ")
-trouve = False
-
-while not trouve:
-    nombre = int(input("Entrez un nombre : "))
-    essais += 1
-    
-if nombre < cache:
-        print("Le nombre cache est plus grand.")
-elif nombre > cache:
-        print("Le nombre cache est plus petit.")
-else:
-        print("Bravo ! Vous avez trouvé le nombre.")
-        print("Nombre à assaiyer:", essais)
-        trouve = True
+devine = random.randint(1, 100)
